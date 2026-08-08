26b90fdfbbd7a7e859f4bb74051987bf7bd2b7fe
= <=>
2026-08-08 22:24:25 +0530

    ........

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -3,8 +3,16 @@ import { getWeatherIcon } from "../services/weatherService";
 import "./DailyForecast.css";
 
 const DailyForecast = ({ forecast = [] }) => {
+  console.log(forecast);
   if (!forecast.length) return null;
+  const formatDate = (date) => {
+    if (!date) return "";
 
+    return new Date(date).toLocaleDateString([], {
+      day: "numeric",
+      month: "short",
+    });
+  };
   return (
     <div className="daily-container">
       <h2 className="daily-title">5-Day Forecast</h2>
@@ -13,8 +21,8 @@ const DailyForecast = ({ forecast = [] }) => {
         {forecast.map((day, index) => (
           <div className="daily-card" key={index}>
             <div className="daily-left">
-              <h3>{day.day}</h3>
-
+              <h3>{day.day}, {formatDate(day.date)}</h3>
+              
               <img
                 src={getWeatherIcon(day.icon)}
                 alt={day.description}
